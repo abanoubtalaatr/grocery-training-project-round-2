7683dccaf3edd75873aeeb91bcd261cabac47aa4
Mohammed <mohamadazizo988@gmail.com>
2026-08-03 17:46:20 +0300

    refactor smartlistlistcntroller file

--- app/Http/Controllers/Api/SmartListListController.php
+++ app/Http/Controllers/Api/SmartListListController.php
@@ -4,23 +4,26 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\SmartList;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Response;
 
 class SmartListListController extends Controller
 {
     public function index()
     {
-        $smartListLists = SmartListList::all();
-        
+        $smartList = SmartList::all();
+
         return response()->json([
             'success' => true,
             'message' => 'Smart List Lists',
-            'data' => SmartListList::all(),
+            'data' => $smartList,
         ]);
     }
 
     public function show(SmartList $smartList)
-    {   
-        if($smartList->user_id !== auth()->user()->id) {
+    {
+        if ($smartList->user_id !== auth()->user()->id) {
             return response()->json([
                 'success' => false,
                 'message' => 'Unauthorized',
@@ -47,7 +50,7 @@ class SmartListListController extends Controller
             'is_pinned' => $request->is_pinned,
             'is_favorite' => $request->is_favorite,
         ]);
-        
+
         return response()->json([
             'success' => true,
             'message' => 'Smart List List created',
@@ -57,22 +60,45 @@ class SmartListListController extends Controller
 
     public function update(Request $request, SmartList $smartList)
     {
-        if($smartList->user_id !== auth()->user()->id) {
+        if ($smartList->user_id !== auth()->user()->id) {
             return response()->json([
                 'success' => false,
                 'message' => 'Unauthorized',
             ], 401);
         }
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'is_active' => 'boolean',
+            'is_public' => 'boolean',
+            'is_deleted' => 'boolean',
+            'is_archived' => 'boolean',
+            'is_pinned' => 'boolean',
+            'is_favorite' => 'boolean',
+        ]);   // or we can make validation like a class in request folder and use it here
+        $smartList->update($validatedData);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Smart List List updated',
+            'data' => $smartList,
+        ]);
     }
 
     public function destroy(SmartList $smartList)
     {
-        if($smartList->user_id !== auth()->user()->id) {
+        if ($smartList->user_id !== auth()->user()->id) {
             return response()->json([
                 'success' => false,
                 'message' => 'Unauthorized',
             ], 401);
         }
-    }
 
+        $smartList->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Smart List List deleted successfully',
+        ]);
+    }
 }
